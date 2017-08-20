9a5df8ab7c41531b5edd6a878341f62988a754b2
Luis Atala <luis.atala@gmail.com>
2017-08-20 03:25:28 -0300

    fix time calculations

--- zklib/zkconst.py
+++ zklib/zkconst.py
@@ -59,19 +59,19 @@ def decode_time(t):
 
     copied from zkemsdk.c - DecodeTime"""
     second = t % 60
-    t = t / 60
+    t = t // 60
 
     minute = t % 60
-    t = t / 60
+    t = t // 60
 
     hour = t % 24
-    t = t / 24
+    t = t // 24
 
     day = t % 31+1
-    t = t / 31
+    t = t // 31
 
     month = t % 12+1
-    t = t / 12
+    t = t // 12
 
     year = t + 2000
 
